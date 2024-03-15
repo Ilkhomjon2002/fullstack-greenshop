96a55eaaee57f2d1c6f46d3fa8ef07f3266acd09
Ilkhomjon2002 <ilhommaxsudov3@gmail.com>
2024-03-15 23:38:49 +0900

    client init

---
+++ client/src/http/http.ts
@@ -0,0 +1,47 @@
+import axios from "axios";
+const baseURL = process.env.REACT_BASE_URL + "/v1";
+
+const instance = axios.create({
+	baseURL,
+});
+
+instance.interceptors.response.use(
+	(response) => {
+		console.log(response.data);
+		return response;
+	},
+
+	async (error) => {
+		if (error.code == "ERR_NETWORK") {
+			return Promise.reject({
+				error: "Network Failed. Please check your connection!",
+				status: "CONERROR",
+			});
+		} else if (error.code == "ECONNABORTED") {
+			return Promise.reject({
+				error: "Connection Time out. Please check your connection!",
+				status: "CONERROR",
+			});
+		} else {
+			const {
+				config,
+				response: { status },
+			} = error;
+			const originalRequest = config;
+
+			let resData: { code: any; error: any; data: any; status: any } = {
+				code: null,
+				error: null,
+				data: null,
+				status: null,
+			};
+
+			resData.code = error.response.data.code;
+			resData.error = error.response.data.message;
+			resData.status = status;
+			return Promise.reject(resData);
+		}
+	}
+);
+
+export default instance;
---
+++ client/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [],
+	theme: {
+		colors: {
+			primary: "rgba(70, 163, 88, 1)",
+			secondary: "rgba(114, 114, 114, 1)",
+		},
+		extend: {},
+	},
+	plugins: [],
+};
